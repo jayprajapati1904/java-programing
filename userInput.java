3ab0e6cda96c102476ffbfe3d01dd68a3e3e0c46
Jay <jay1904prajapati@gmail.com>
2024-11-23 00:13:01 +0530

    normal user input

---
+++ userInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class userInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your name :");
+        String name = sc.nextLine();
+        System.out.println("welcomme " + name + " my programing journey...!");
+
+    }
+}
